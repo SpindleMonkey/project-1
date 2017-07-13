90ff4c6b353f088598ba5ead2311629eaa435b37
connie kephart <spindle.monkey@gmail.com>
2017-07-13 15:11:01 -0600

    moves teams to an array and starts building game logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
-let teamOne;
-let teamTwo;
+let theTeams = [];
 
 let defense;  // determines who's turn it is
 
@@ -31,7 +30,7 @@ const playerImages = [
 // the shadows of the offensive players look the same, and are defined by CSS
 let shadowPlayer = document.createElement('div');
 shadowPlayer.className = 'playerShadow';
-console.log(shadowPlayer);
+//console.log(shadowPlayer);
 
 /**
  * Use random numbers to pick images for the players
@@ -50,9 +49,9 @@ function Player(name) {
 
   var whichImage = getRandom();
 
-  this.image = playerImages[whichImage][0];
-  this.width =  playerImages[whichImage][1];
-  this.height = playerImages[whichImage][2];
+  this.image = playerImages[whichImage][0];   // image patah
+  this.width =  playerImages[whichImage][1];  // image width
+  this.height = playerImages[whichImage][2];  // image height
   //console.log(this);
 }
 
@@ -67,11 +66,10 @@ Player.prototype.leaveTeam = function() {
 /**
  * Team object definition
  */
-function Team(id, name, size, color) {
+function Team(id, name, size) {
   this.id = id;
   this.name = name;
   this.size = size;
-  this.color = color; //TODO: remove this!
 
   this.stash = 1;
   this.players = [];
@@ -110,7 +108,13 @@ Team.prototype.showLineup = function() {
     newPlayer = document.createElement('li');
     newPlayer.textContent = this.players[i].name;
     playerList.append(newPlayer);
-    //console.log(playerList);
+
+    // TEST TEST TEST
+    newPlayer = document.createElement('li');
+    newPlayer.textContent = this.players[i].name + " II";
+    playerList.append(newPlayer); 
+
+    console.log(playerList);
   }
   myAside.append(playerList);
 
@@ -118,23 +122,23 @@ Team.prototype.showLineup = function() {
 
 Team.prototype.showTeam = function(defense) {
   if (defense === this.id) {
-    console.log("team" + this.id + " is on defense");
+   // console.log("team" + this.id + " is on defense");
 
     let playerList = document.getElementById('defense');
 
     // append each player's image 
     for (let i = 0; i < this.size; i++) {
-      console.log("ack! add the right image here!");
+      //console.log("ack! add the right image here!");
       let newGuy = document.createElement('img');
       newGuy.setAttribute("src", this.players[i].image);
       newGuy.setAttribute("alt", this.players[i].name);
       newGuy.setAttribute("width", this.players[i].width);
       newGuy.setAttribute("height", this.players[i].height);
-      console.log(this.players[i]);
-      console.log(newGuy);
+      //console.log(this.players[i]);
+      //console.log(newGuy);
       playerList.append(newGuy);
     }
-    console.log("defense: " + playerList);
+    //console.log("defense: " + playerList);
   } else {
     //console.log("team" + this.id + " is on offense");
 
@@ -145,7 +149,7 @@ Team.prototype.showTeam = function(defense) {
       let newShadow = shadowPlayer.cloneNode(true);
       playerList.append(newShadow);
     } 
-    console.log("offense: " + playerList);
+    //console.log("offense: " + playerList);
   }
 };
 
@@ -208,25 +212,95 @@ function initGame() {
 
   // create the two teams
   let team = cleanUpName(getQueryValue('team1'));
-  teamOne = new Team(1, (team ? team : 'team 1'), teamSize, 'Navy');  // TODO: 
-  //console.log(teamOne);
+  theTeams[0] = new Team(1, (team ? team : 'team 1'), teamSize);  
+  //console.log(theTeams[0]);
 
   team = cleanUpName(getQueryValue('team2'));
-  teamTwo = new Team(2, (team ? team : 'team 2'), teamSize, 'OrangeRed');
-  //console.log(teamTwo);
+  theTeams[1] = new Team(2, (team ? team : 'team 2'), teamSize);
+  //console.log(theTeams[1]);
 
-  // build the players (use a different set of names, 0 or 1, for each team)
-  teamOne.buildTeam(0);
-  teamTwo.buildTeam(1);
+  //console.log("theTeams.length: " + theTeams.length);
 
-  // show the team lineups on the gamefield
-  teamOne.showLineup();
-  teamTwo.showLineup();
-  
   // set the starting team (currently defaults to team1):
   defense = 1;
 
-  teamOne.showTeam(defense);
-  teamTwo.showTeam(defense);
+  for (let i = 0; i < theTeams.length; i++) {
+    // build the players (use a different set of names, 0 or 1, for each team)
+    theTeams[i].buildTeam(i);
 
+    // show the team lineups on the gamefield
+    theTeams[i].showLineup();
+
+    // TEST TEST TEST
+    theTeams[i].showTeam(defense);
+
+  }
+
+  //playGame();
+}
+
+function gameOver() {
+  // check team sizes! If a team has no more members, it lost (which means the other 
+  // team won and the game is over!)
+  // but if both teams still have at least 1 member, the game is not over
+
+  if (theTeams[0].size === 0) {
+    // Aww! this team lost and the other team won!
+    console.log("Team" + theTeams[1].id + " won!");
+    return theTeams[1].id;
+  } else if (theTeams[1].size === 0) {
+    console.log("Team" + theTeams[0].id + " won!");
+    return theTeams[0].id;
+  }
+
+  console.log("the game is (still) on");
+  return false; // the game is on!
+}
+
+function buildDropdown(aTeam) {
+  /**
+   *  this function builds a dropdown, and populates it
+   * with the names of the players belonging to the
+   * aTeam Team object
+   *
+   * returns: the dropdown element
+   */
+  var dropdown = document.createElement('select');
+  for (let i = 0; i < aTeam.size; i++) {
+    var newOption = document.createElement('option');
+    newOption.setAttribute("value", aTeam.players[i].name);
+    newOption.textContent = aTeam.players[i].name;
+    //console.log(newOption);
+    dropdown.append(newOption);
+  }
+  
+  //console.log(dropdown);
+  return dropdown;
+}
+
+function playGame() {
+  // while their is no winner
+  //   update the gamefield with the teams
+  //   defense calls for a player
+  //   if offense has a tool and offense wants to use tool
+  //     offense uses tool
+  //     update defense team status
+  //   else
+  //     offense sends player
+  //     if offense breaks through
+  //       offense picks a defense player to add to their team
+  //     else 
+  //       defense adds offensive payer to their team
+  //   if there is a winner
+  //     show winner banner with team namd and player names
+  // endwhile
+
+  while (!gameOver()) {
+
+    for (let i = 0; i < theTeams.length; i++) {
+      // update the teams on the field
+      theTeams[i].showTeam(defense);
+      theTeams[i].makeMove(defense);
+    }
+  }
 }
